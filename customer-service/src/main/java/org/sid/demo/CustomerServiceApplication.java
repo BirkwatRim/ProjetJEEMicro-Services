51a948b631694213b3a05c1a52d27a079504ae64
Rim Birkwat <rim.b2072015@gmail.com>
2021-01-16 16:20:07 +0100

    first-commit

---
+++ customer-service/src/main/java/org/sid/demo/CustomerServiceApplication.java
@@ -0,0 +1,32 @@
+package org.sid.demo;
+
+import org.sid.demo.entities.Customer;
+import org.sid.demo.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository, RepositoryRestConfiguration restConfiguration){
+        restConfiguration.exposeIdsFor(Customer.class);
+        return args ->{
+
+            customerRepository.save(new Customer(null,"Rim","birkwatr@gmail.com"));
+            customerRepository.save(new Customer(null,"Anas","Anasb@gmail.com"));
+            customerRepository.save(new Customer(null,"Chaimaa","bchaimaa@gmail.com"));
+            customerRepository.findAll().forEach(c->{
+                System.out.println(c.toString());
+            });
+        };
+    }
+}
